7ab9f378018a9e4f5791b59e617b7b7bfaedead4
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2023-09-30 23:02:37 -0700

    Crying sobbing because node simply won't run with express for some reason: pivoting to python and flask

--- backend/routes/user-routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import getAllUsers from "./controllers/user-controller.js";
-
-const userRouter = express.Router();
-
-// Route to get all users
-userRouter.get("/", getAllUsers);
-
-export default userRouter;
