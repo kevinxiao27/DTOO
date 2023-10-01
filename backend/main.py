f39502dc93461eb232ffacfab3ca5f4f3c3f4d79
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2023-10-01 01:02:04 -0700

    Added classes for outfits and getAllOutfits and getOutfitById

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask("Outfit")
 api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument("Tops", required=True)
+# go through and add parser for every category
 
 
 outfits = {
@@ -19,11 +23,28 @@ outfits = {
 }
 
 
+class completeOutfit():
+    def __init__(self, tops=dict, bottoms=dict, outerwear=dict, footwear=dict, accessories=dict):
+        self.tops = tops
+        self.bottoms = bottoms
+        self.outerwear = outerwear
+        self.footwear = footwear
+        self.accessories = accessories
+    # tops is tops = {"colour": x, "brand": y}
+    # repeat this for rest of categories in completeOutfit
+
+
 class Outfit(Resource):
     def get(self, outfit_id):
-        if outfit_id == "":
+        if outfit_id == "all":
             return outfits
         return outfits[outfit_id]
+
+    def put(self, outfit_id):
+        args = parser.parse_args()
+        new_outfit = {'tops': args['tops']}
+        videos[video_id] = new_video
+        return {}
 
 
 api.add_resource(Outfit, '/outfits/<outfit_id>')
