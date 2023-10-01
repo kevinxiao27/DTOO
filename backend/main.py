aecd152f191e25b05ffcbb44f3be00b8fd079854
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2023-10-01 00:34:00 -0700

    Created basic get call for REST api

--- backend/main.py
+++ backend/main.py
@@ -5,18 +5,28 @@ app = Flask("Outfit")
 api = Api(app)
 
 
-class Outfit(Resource):
-
-    def get(self):
-        return "hello world"
-
-
 outfits = {
-    'outfit1': {"Tops": "Nike Cap"}
+    'outfit1': {"Tops": "V Neck Shirt",
+                "Bottoms": "Pleated pants",
+                "Outerwear": "Denim Jacket",
+                "Footwear": "New Balances",
+                "Accessories": "Chain"},
+    'outfit2': {"Tops": "White Turtle Neck",
+                "Bottoms": "Pleated pants",
+                "Outerwear": "Rain Jacket",
+                "Footwear": "New Balances",
+                "Accessories": "Rings"},
 }
 
 
-api.add_resource(Outfit, '/')
+class Outfit(Resource):
+    def get(self, outfit_id):
+        if outfit_id == "":
+            return outfits
+        return outfits[outfit_id]
+
+
+api.add_resource(Outfit, '/outfits/<outfit_id>')
 
 if __name__ == '__main__':
     app.run()
